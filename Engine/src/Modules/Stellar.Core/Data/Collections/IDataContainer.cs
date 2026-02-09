692bfb031da78e53a23b747acfd4b3fe4d4dff11
MarcT1me <marctimeproger@gmail.com>
2026-02-09 18:35:54 +0700

    [SC] adding IDataContainer interface

---
+++ Engine/src/Modules/Stellar.Core/Data/Collections/IDataContainer.cs
@@ -0,0 +1,8 @@
+using Stellar.Kernel.Quantization;
+
+namespace Stellar.Core.Data.Collections;
+
+[Obsolete("The class is mainly used in the engine, you should not use it in the game.")]
+public interface IDataContainer : IQuant
+{
+}
